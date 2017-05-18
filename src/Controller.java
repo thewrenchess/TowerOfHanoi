1226b2391b14ea22ed6144e3e937ac6011ed5ea2
thewrenchess <ydxu2147483648>
2017-05-18 16:12:14 -0700

    Solution added and debugged

--- src/Controller.java
+++ src/Controller.java
@@ -3,17 +3,16 @@
  */
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class	Controller {
 	private int			size = 0;
-	private Game			game = new Game();
+	private Game		game = new Game();
+	private Scanner		scan = new Scanner(System.in);
 
 	private int			mainMenu() {
-		Scanner		scan = new Scanner(System.in);
-		
 		while (size < 1 || size > 30) {
-			System.out.println("Welcome to the Towers of Hanoi game.");
-			System.out.print("Please enter an integer (between 1 and 30) of disks you would like to play with, or 'exit' to exit ");
+			System.out.print("Please enter an integer (between 1 and 30) representing the amount of disks you would like to play with, or 'exit' to exit ");
 			String	input = scan.nextLine();
 			if (input.equals("exit"))
 				return 0;
@@ -27,20 +26,46 @@ public class	Controller {
 		}
 		return size;
 	}
-
-	public static void	main(String[] args) {
-		Controller	c = new Controller();
-		int			loop = 1;
-
+	private int			solutionOrGame() {
+		int	loop = 1;
+	
+		System.out.println(	"\nEnter 'play' to play the Tower of Hanoi game," +
+							"or enter 'solution' to see solution to current towers");
 		while (loop == 1) {
-			if (c.mainMenu() == 0) {
-				System.out.println("Exiting...");
+			String	input = scan.nextLine();
+			if (input.equals("play"))
+				return 1;
+			if (input.equals("solution"))
+				return 2;
+			System.out.println("\n----------------------------------------");
+			System.out.println("Please enter either 'play' or 'solution'");
+			System.out.println("----------------------------------------\n");
+		}
+		return 0;
+	}
+	
+	public static void	main(String[] args) throws InterruptedException {
+		Controller	c = new Controller();
+		int			loop = 1, result;
+
+		System.out.println("Welcome to the Towers of Hanoi game.\n");
+		TimeUnit.SECONDS.sleep(1);
+		System.out.println("The goal of this game is to move the entire stack of disks from tower A to tower C.\n");
+		TimeUnit.SECONDS.sleep(1);
+		while (loop == 1) {
+			result = c.mainMenu();
+			if (result == 0) {
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println("Thank you for playing Tower of Hanoi.\nExiting...");
 				return ;
 			}
-			if (c.game.menu(c.size) == 0) {
-				System.out.println("Exiting...");
-				return ;
+			result = c.solutionOrGame();
+			if (result == 1) {
+				c.game.menu(c.size);
+			}else if (result == 2) {
+				c.game.solution(c.size);
 			}
+			c.size = 0;
 		}
 	}
 
--- src/Game.java
+++ src/Game.java
@@ -3,24 +3,24 @@
  */
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class Game {
 	
 	private Towers	towers;
+	private Scanner	scan = new Scanner(System.in);
 	
 	private void	printrules() {
-		System.out.println("\n\nRules:");
+		System.out.println("\n\nGoal:");
+		System.out.println("-You goal is to move the entire stack of disks from tower A to tower C.\n");
+		System.out.println("\nRules:");
 		System.out.println("-Only one disk can be moved at a time.");
 		System.out.println("-Each move consists of taking the upper disk from one of the stacks and placing it on top of another stack");
 		System.out.println("-No disk may be placed on top of a smaller disk.");
-		System.out.println("-You goal is to move the entire stack to tower C.\n");
 	}
-	
-	
 	
 	private int		getChoice() {
 		int		choice = 0;
-		Scanner	scan = new Scanner(System.in);
 		
 		while (choice < 1 || choice > 3) {
 			System.out.println("\nWhat would you like to do next?");
@@ -62,12 +62,14 @@ public class Game {
 			} else if (towers.towerB.get(towers.towerB.size() - 1).length() < towers.towerC.get(towers.towerC.size() - 1).length()) {
 				System.out.println("(" + i + ")Move disk from B to C");
 			}
-			System.out.println("Enter the integer representing move, 'restart' to restart the game, 'exit' to exit");
+			System.out.println("Enter the integer representing move, 'rules' to see rules, 'restart' to restart the game, 'exit' to exit");
 			String	input = scan.nextLine();
 			if (input.equals("exit"))
 				return 0;
 			if (input.equals("restart"))
 				return -1;
+			if (input.equals("rules"))
+				return -2;
 			if (input.matches("[0-9]+"))
 				choice = Integer.parseInt(input);
 			if (choice < 1 || choice > 3) {
@@ -79,16 +81,92 @@ public class Game {
 		return choice;
 	}
 	
-	public int	menu(int size) {
+	private void	solveEven() throws InterruptedException {
+		if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
+			return ;
+		
+		towers.moveBetweenAAndB();
+		towers.printTowers();
+		System.out.println();
+		TimeUnit.SECONDS.sleep(2);
+		if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
+			return ;
+		towers.moveBetweenAAndC();
+		towers.printTowers();
+		System.out.println();
+		TimeUnit.SECONDS.sleep(2);
+		if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
+			return ;
+		towers.moveBetweenBAndC();
+		towers.printTowers();
+		System.out.println();
+		TimeUnit.SECONDS.sleep(2);
+		if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
+			return ;
+		solveEven();
+	}
+	
+	private void solveOdd() throws InterruptedException {
+		towers.moveBetweenAAndC();
+		towers.printTowers();
+		System.out.println();
+		TimeUnit.SECONDS.sleep(2);
+		if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
+			return ;
+		towers.moveBetweenAAndB();
+		towers.printTowers();
+		System.out.println();
+		TimeUnit.SECONDS.sleep(2);
+		if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
+			return ;
+		towers.moveBetweenBAndC();
+		towers.printTowers();
+		System.out.println();
+		TimeUnit.SECONDS.sleep(2);
+		if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
+			return ;
+		solveOdd();
+	}
+	
+	public void		solution(int size) throws InterruptedException {
+		String	input;
+		
+		System.out.println("Would you like to see the solution?(y/n)");
+		input = scan.nextLine();
+		if (input.equals("y")) {
+			towers = new Towers(size);
+			towers.printTowers();
+			System.out.println();
+			TimeUnit.SECONDS.sleep(2);
+			if (size % 2 == 0)
+				solveEven();
+			else
+				solveOdd();
+		}
+	}
+	
+	public void		menu(int size) throws InterruptedException {
 		towers = new Towers(size);
 		int	loop = 1, choice;
 		
-		while (loop == 1) {
-			printrules();
+		System.out.println("Settint up new Towers...");
+		TimeUnit.SECONDS.sleep(1);
+		while (loop > 0) {
+			if (loop < 2) {
+				TimeUnit.SECONDS.sleep(1);
+				printrules();
+				TimeUnit.SECONDS.sleep(1);
+				loop++;
+			}
+			System.out.println();
 			towers.printTowers();
+			System.out.println();
 			choice = getChoice();
 			if (choice == -1) {
+				System.out.println("Settint up new Towers...");
 				towers = new Towers(size);
+			} else if (choice == -2) {
+				loop--;
 			} else if (choice == 1) {
 				towers.moveBetweenAAndB();
 			} else if (choice == 2) {
@@ -97,13 +175,21 @@ public class Game {
 				towers.moveBetweenBAndC();
 			}
 			if (choice == 0)
-				return 0;
+				return ;
 			if (towers.towerA.size() == 0 && towers.towerB.size() == 0)
 				loop = 0;
 		}
 		System.out.println();
 		towers.printTowers();
-		System.out.println("\nSuccessful! You used " + towers.steps + " steps, and ideally it takes only " + (int)(Math.pow(2, size) - 1) + " steps");
-		return 0;
+		TimeUnit.SECONDS.sleep(1);
+		System.out.println(	"\nSuccessful! You used "
+							+ towers.steps
+							+ " steps, and ideally it takes only "
+							+ (int)(Math.pow(2, size) - 1)
+							+ " steps.\n");
+		System.out.println();
+		TimeUnit.SECONDS.sleep(1);
+		solution(size);
+		System.out.println();
 	}
 }
